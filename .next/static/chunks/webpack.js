392a3d34076db09ea8a555272dcddfa46b9b2d88
AndrewMalcher <andrew.malcher.r@gmail.com>
2024-10-17 18:00:08 -0400

    chore: change script dev, to run 'npx prisma generate && next dev'

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0fbc65830a41bfa0"; }
+/******/ 		__webpack_require__.h = function() { return "0103b09343822f2c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
