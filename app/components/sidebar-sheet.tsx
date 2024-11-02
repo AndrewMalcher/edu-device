1f20b261ced85983fcd5f840b500552d34bfce50
AndrewMalcher <andrew_malcher_@hotmail.com>
2024-11-02 15:22:57 -0400

    feat: login initial

--- app/components/sidebar-sheet.tsx
+++ app/components/sidebar-sheet.tsx
@@ -1,8 +1,16 @@
-import { Avatar, AvatarImage } from "./ui/avatar"
-import { HomeIcon, CalendarIcon, LogOutIcon } from "lucide-react"
+import { HomeIcon, CalendarIcon, LogOutIcon, LogInIcon } from "lucide-react"
 import { Button } from "./ui/button"
 import { SheetContent, SheetHeader, SheetTitle, SheetClose } from "./ui/sheet"
 import Link from "next/link"
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/app/components/ui/dialog"
+import Image from "next/image"
 
 const SidebarSheet = () => {
   return (
@@ -11,15 +19,41 @@ const SidebarSheet = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="boder-solid flex items-center gap-3 border-b py-5">
-        <Avatar>
-          <AvatarImage src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXZhdGFyfGVufDB8fDB8fHww" />
-        </Avatar>
+      <div className="boder-solid flex items-center justify-between gap-3 border-b py-5">
+        <h2 className="text-lg font-bold">Olá, Faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma.</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta Google.
+              </DialogDescription>
+            </DialogHeader>
+            <Button>
+              <Image
+                src="\public\Google.png"
+                alt="Google"
+                width={18}
+                height={18}
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+
+        {/* Avatar
+          AvatarImage src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXZhdGFyfGVufDB8fDB8fHww" />
+        /Avatar
 
         <div>
           <p className="font-bold">Joe Miguer</p>
           <p className="text-xs">joe.miguer@faculdadelasalle.edu.br</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-4 border-b border-solid py-5">
